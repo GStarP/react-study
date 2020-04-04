a442ce52de0381e6e5ac251dfbda47a1428904e5
GStarP <171250605@smail.nju.edu.cn>
2020-04-04 11:36:24 +0800

    feat: hoc

--- src/index.js
+++ src/index.js
@@ -1,146 +1,86 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-// function component
-function Square(props) {
+class DataSource {
+  comments = [1, 2, 3]
+  users = ['hxw', 'zzn', 'lxc']
+  listeners = []
+  registerChangeListener = (callback) => {
+    this.listeners.push(callback)
+  }
+  triggerChange = () => {
+    this.listeners.forEach(c => {
+      c()
+    })
+  }
+}
+
+const datasource = new DataSource()
+
+function subscribe(WrappedComponent, getData) {
+  return class extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        data: getData(datasource)
+      }
+    }
+
+    componentDidMount() {
+      datasource.registerChangeListener(() => {
+        this.setState({
+          data: getData(datasource)
+        })
+      })
+    }
+
+    render() {
+      return <WrappedComponent {...this.props} data={this.state.data} />
+    }
+  }
+}
+
+function CommontList(props) {
   return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
+    <ul>
+      {props.data.map(c => <li key={c}>{c}</li>)}
+    </ul>
   )
 }
 
-class Board extends React.Component {
-
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    );
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
+function UserList(props) {
+  return (
+    <div>
+      {props.data.map(u => <div key={u}>{u}</div>)}
+    </div>
+  )
 }
 
-class Game extends React.Component {
+const CommentListWithSub = subscribe(CommontList, (source) => source.comments)
+const UserListWithSub = subscribe(UserList, (source) => source.users)
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      history: [{
-        squares: Array(9).fill(null)
-      }],
-      next: 'X',
-      stepNumber: 0
-    };
-  }
+class Page extends React.Component {
 
-  calculateWinner = (squares) => {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
-
-  clickHandler = (i) => {
-    const his = this.state.history.slice(0, this.state.stepNumber + 1);
-    const tmp = his[his.length - 1].squares;
-    if (this.calculateWinner(tmp) || tmp[i]) {
-      return;
-    }
-    tmp[i] = this.state.next;
-    this.setState({
-      history: this.state.history.concat([{
-        squares: tmp
-      }]),
-      next: this.state.next === 'X' ? 'O' : 'X',
-      stepNumber: this.state.history.length
-    });
-  }
-
-  jumpTo(step) {
-    this.setState({
-      stepNumber: step,
-      next: (step % 2) === 0 ? 'X' : 'O'
-    })
+  changeDataSource() {
+    datasource.comments[0] = datasource.comments[0] === 1 ? 5 : 1
+    datasource.users[0] = datasource.users[0] === 'hxw' ? 'dyx' : 'hxw'
+    datasource.triggerChange() 
   }
 
   render() {
-    const curSquare = this.state.history[this.state.stepNumber].squares;
-    
-    const win = this.calculateWinner(curSquare);
-    let status;
-    if (win) {
-      status = `Winner: ${win}`
-    } else {
-      status = `Next player: ${this.state.next}`;
-    }
-
-    const moves = this.state.history.map((step, move) => {
-      const desc = move ? `Go to move #${move}` : 'Go to game start';
-      return (
-        <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
-        </li>
-      )
-    })
-
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board 
-            squares={curSquare}
-            onClick={(i) => this.clickHandler(i)}
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
-        </div>
-      </div>
-    );
+      <>
+        <CommentListWithSub/>
+        <UserListWithSub />
+        <button onClick={() => this.changeDataSource()}>Change</button>
+      </>
+    )
   }
 }
 
 // ========================================
 
 ReactDOM.render(
-  <Game />,
+  <Page />,
   document.getElementById('root')
 );
